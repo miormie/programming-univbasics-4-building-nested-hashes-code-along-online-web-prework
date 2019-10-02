aaf21f5d436dca9fa443b6a2712fb8bbfd2fa5e5
Rose Seraphin <roseseraphin@yahoo.com>
2019-10-02 01:40:14 +0000

    Done.

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,11 +1,29 @@
 def second_challenge
-  epic_tragedy = {
+epic_tragedy = {
     :montague => {
+    :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => []
+      
       #your key/value pairs here!
+      # first 3 keys point to a value of an empty hash 
+      # 4th key(hero_friends) has a value of an empty array 
       }, 
     :capulet => {
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
+      
+      
+      
       #your key/value pairs here!
-      }}
+      # first 3 keys point to a value of an empty hash 
+      # 4th key(hero_friends) has a value of an empty array 
+      }
+    
+  }
 
   
-end
+End
