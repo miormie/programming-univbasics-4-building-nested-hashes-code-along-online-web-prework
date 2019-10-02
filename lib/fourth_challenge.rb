aaa011dac8bc494428eb09f357d95f6c46dfa27f
Rose Seraphin <roseseraphin@yahoo.com>
2019-10-02 01:48:33 +0000

    Done.

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -5,8 +5,8 @@ def fourth_challenge
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
-        {name: "Benvolio", age: "17", attitude: "worrried"},
-        {name: "Mercutio", age: "18", attiude: "hot-headed"}
+        {name: "Benvolio", age: "17", attitude: "worried"},
+        {name: "Mercutio", age: "18", attitude: "hot-headed"}
         
         
         #your hashes here!
